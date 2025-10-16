a2d66648dfe6b6c89a37e1fe94d481ee246a5e85
renothetrader <126786731+renothetrader@users.noreply.github.com>
2025-10-16 23:04:57 +0400

    Add App.js and update index.js for proper React app entry

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import RecruitmentATS from '../RecruitmentATS.jsx';
+import App from './App';
 
 ReactDOM.render(
-  <RecruitmentATS />,
+  <App />,
   document.getElementById('root')
 );
